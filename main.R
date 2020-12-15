d1773b303ad12436662e8aeb20604f5e5975d969
JinyanYang <jyyoung@live.com>
2020-12-15 15:07:45 +1100

    bugs fixed; date from read_exif needs to be converted

--- main.R
+++ main.R
@@ -16,19 +16,21 @@ pot.nm <- unique(aoi.df$Pot_ID)[1]#you will need to loop
 aoi.pot <- aoi.df[aoi.df$Pot_ID==pot.nm,]
 
 # read in picture date 
-pic.folder <- sprintf('pic/%s/',pot.nm)
+pic.folder <- 'data/Pictures/1/'# sprintf('pic/%s/',pot.nm)
 
-pictures.fn <- list.files(pic.folder,pattern = '.jpg',full.names = T)
+pictures.fn <- list.files(pic.folder,pattern = '.JPG',full.names = T)
 
 # loop through the pictures
 tmp.ls <- list()
 for (i in seq_along(pictures.fn)) {
   
-  date.pic<- read_exif(fn.vec)$CreateDate
+  date.pic<- read_exif(pictures.fn[i])$CreateDate
+  date.pic <- as.Date(date.pic,'%Y:%m:%d')
+  ROIDates <- aoi.pot$Date
   index.aoi <- ROI.Date.func(dd=date.pic,n=length(aoi.pot$Date))
   
   aoi.pot.xy <- aoi.pot[index.aoi,c('Xmin','Xmax','Ymin','Ymax')]
-  
+  aoi.pot.xy <- as.numeric(aoi.pot.xy)
   tmp.df <- processImage.new(pictures.fn[i],ROI = aoi.pot.xy)
   tmp.df$Date <- date.pic
   
@@ -36,3 +38,5 @@ for (i in seq_along(pictures.fn)) {
 }
 
 pot.gcc.df <- do.call(rbind,tmp.ls)
+
+pot.gcc.df$Pot_ID <- pot.nm
